7c744fc324ac267c2f0b74c10cffa8d340b21f82
WoodManGitHub <woodmanpublic@gmail.com>
2020-02-28 22:25:58 +0800

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+
+  ignorePatterns: [
+    'node_modules/',
+    'src/assets/'
+  ],
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/camelcase': 'off',
+    '@typescript-eslint/camelcase': 'off'
+  }
+}
