9adb52bf8d71c1cadb4232f8b3ea3ef11edcbe19
WoodManGitHub <woodmanpublic@gmail.com>
2020-03-02 23:53:55 +0800

    Remove debug

--- src/plugins/axios/api.ts
+++ src/plugins/axios/api.ts
@@ -1,4 +1,4 @@
 import http from './axios'
 export default {
-  get: (type = '', serverID = '') => http.Get(`http://localhost:8788/api/${type}/${serverID}`)
+  get: (type = '', serverID = '') => http.Get(`http://localhost:8787/api/${type}/${serverID}`)
 }
