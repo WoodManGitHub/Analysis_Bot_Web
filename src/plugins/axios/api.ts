c2ca9f8bd5de768923d3c11c56b3787cab0e1f41
WoodManGitHub <woodmanpublic@gmail.com>
2020-03-03 10:27:56 +0800

    Fix api bug

--- src/plugins/axios/api.ts
+++ src/plugins/axios/api.ts
@@ -1,4 +1,4 @@
 import http from './axios'
 export default {
-  get: (type = '', serverID = '') => http.Get(`http://localhost:8787/api/${type}/${serverID}`)
+  get: (type = '', serverID = '') => http.Get(`/api/${type}/${serverID}`)
 }
