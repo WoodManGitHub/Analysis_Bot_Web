b119976401b8640472acfe66bc22e3e8d913dfd1
WoodManGitHub <woodmanpublic@gmail.com>
2020-03-10 20:46:27 +0800

    Add index page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,16 @@ import Router from 'vue-router'
 import day from '@/components/day.vue'
 import month from '@/components/month.vue'
 import all from '@/components/all.vue'
+import index from '@/components/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
+    path: '/',
+    name: 'index',
+    component: index
+  }, {
     path: '/day/:serverID',
     name: 'day',
     component: day
