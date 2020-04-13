6b87aa78f862f1226fc6fde31739ea7247c027de
WoodManGitHub <woodmanpublic@gmail.com>
2020-04-13 20:11:30 +0800

    Fix api url

--- src/plugins/axios/api.ts
+++ src/plugins/axios/api.ts
@@ -1,5 +1,5 @@
 import http from './axios'
 export default {
   get: (type = '', serverID = '') => http.Get(`/api/${type}/${serverID}`),
-  verifyToken: (token = '') => http.Get(`/verify/${token}`)
+  verifyToken: (token = '') => http.Get(`/api/verify/${token}`)
 }
